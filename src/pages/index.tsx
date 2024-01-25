79ee73dbc65e259c99cfd54711f462c1aa2be1b4
brightzoe <brightzoe@qq.com>
2024-01-25 16:06:18 +0800

    chore: adjust the footer in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,23 @@ import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageContent from '../components/HomepageContent';
+import {
+  useThemeConfig,
+  isMultiColumnFooterLinks,
+} from '@docusaurus/theme-common';
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header
+      className={clsx(
+        'hero hero--primary',
+        styles.heroBanner,
+        'text-gradient bg-gradient-to-r from-purple-500 to-indigo-600',
+      )}
+    >
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -17,6 +28,35 @@ function HomepageHeader() {
   );
 }
 
+function RelatedBlogs() {
+  const { footer } = useThemeConfig();
+  if (!footer) {
+    return null;
+  }
+  const { links } = footer;
+
+  return isMultiColumnFooterLinks(links) ? null : (
+    <div className="container mx-auto px-4 py-10">
+      <h2 className="text-2xl font-bold mb-4 color-primary">Related Blogs</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {links.map((blog, i) => (
+          <div
+            onClick={() => window.open(blog.href, '_blank')}
+            key={blog.label}
+            className="cursor-pointer p-6 rounded-lg shadow-lg flex items-start hover:shadow-xl transition-shadow duration-300 ease-in-out dark:shadow-gray-700/50"
+          >
+            <div className="text-2xl mr-4">{i % 2 === 0 ? '🌐 ' : '⚡'}</div>
+            <div>
+              <h3 className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">
+                {blog.label}
+              </h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -25,9 +65,10 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
+      <main className="flex-grow">
         <HomepageContent />
       </main>
+      <RelatedBlogs />
     </Layout>
   );
 }
---
+++ src/theme/Footer/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {useThemeConfig} from '@docusaurus/theme-common';
+import FooterLogo from '@theme/Footer/Logo';
+import FooterCopyright from '@theme/Footer/Copyright';
+import FooterLayout from '@theme/Footer/Layout';
+
+function Footer(): JSX.Element | null {
+  const {footer} = useThemeConfig();
+  if (!footer) {
+    return null;
+  }
+  const {copyright, links, logo, style} = footer;
+
+  return (
+    <>
+    <FooterLayout
+      style={style}
+      links={<></>}
+      logo={logo && <FooterLogo logo={logo} />}
+      copyright={copyright && <FooterCopyright copyright={copyright} />}
+    />
+    </>
+  );
+}
+
+export default React.memo(Footer);
